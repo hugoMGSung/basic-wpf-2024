c85c92d8d2a1fc0d8cad8a75ba3f5a7077b5844b
hugoMGSung <personar95@naver.com>
2024-04-29 17:29:16 +0900

    몰랑

--- day01/wpf01_basic_app/ex03_wpf_again/MainWindow.xaml.cs
+++ day01/wpf01_basic_app/ex03_wpf_again/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ex03_wpf_again
 {
@@ -31,13 +32,59 @@ namespace ex03_wpf_again
             MessageBox.Show(PsbTemp.Password);
         }
 
+        DispatcherTimer timerVideoTime;
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            string mediaFileName = "sample.mp4";
-            string mediaDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            //MessageBox.Show(mediaDirectory);
-            string mediaFilePath = System.IO.Path.Combine(mediaDirectory, "Videos", mediaFileName);
-            MdeMovie.Source = new Uri(mediaFilePath);
+            timerVideoTime = new DispatcherTimer();
+            timerVideoTime.Interval = TimeSpan.FromSeconds(0.1);
+            timerVideoTime.Tick += TimerVideoTime_Tick;
+            MdeMovie.Stop();
+        }
+
+        private void TimerVideoTime_Tick(object? sender, EventArgs e)
+        {
+            ShowPosition();
+        }
+
+        private void MdeMovie_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            sbarPosition.Minimum = 0;
+            sbarPosition.Maximum =
+                MdeMovie.NaturalDuration.TimeSpan.TotalSeconds;
+            sbarPosition.Visibility = Visibility.Visible;
+        }
+
+        private void ShowPosition()
+        {
+            sbarPosition.Value = MdeMovie.Position.TotalSeconds;
+            TxtPosition.Text =
+                MdeMovie.Position.TotalSeconds.ToString("0.0");
+        }
+
+        private void EnableButtons(bool is_playing)
+        {
+            if (is_playing)
+            {
+                BtnCheck.IsEnabled = false;
+                BtnToggle.IsEnabled = true;
+                BtnPlay.Opacity = 0.5;
+                BtnStop.Opacity = 1.0;
+            }
+            else
+            {
+                BtnCheck.IsEnabled = true;
+                BtnToggle.IsEnabled = false;
+                BtnPlay.Opacity = 1.0;
+                BtnStop.Opacity = 0.5;
+            }
+            timerVideoTime.IsEnabled = is_playing;
+        }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            MdeMovie.Play();
+            EnableButtons(true);
         }
     }
 }
